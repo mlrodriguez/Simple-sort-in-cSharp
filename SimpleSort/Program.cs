2e187a8831dd838a5d682880a95dc35cb61beb24
Marcio Rodriguez <marciorodriguez91@gmail.com>
2015-04-18 21:42:35 -0300

    init

---
+++ SimpleSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Program {
+    static void Main(string[] args) {
+        int[] array = {2,4,8,5,465,678,9,345,234,234,467,78,3,2,4,6,7,89,-10};
+        Sort.simpleSort(array);
+        Console.WriteLine(string.Join(" ", array));
+        Console.Read();
+    }
+
+}
+
---
+++ SimpleSort/Sort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public static class Sort {
+    public static void simpleSort(int[] array) {
+        for (int i = 0; i < array.Length-1; i++) {
+            for (int j = i + 1; j < array.Length; j++) {
+                if (array[j] < array[i]) {
+                    int aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                }
+
+            }           
+        }
+    }
+
+    
+}
+
