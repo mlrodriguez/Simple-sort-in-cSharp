3fc6e0d6dcb4f9bff218a305bc786c5d9150560d
Marcio <marciorodriguez91@gmail.com>
2016-05-18 16:45:16 -0300

    autoScape

--- SimpleSort/Program.cs
+++ SimpleSort/Program.cs
@@ -10,7 +10,7 @@ class Program {
         int[] array = {2,4,8,5,465,678,9,345,234,234,467,78,3,2,4,6,7,89,-10};
         Sort.simpleSort(array);
         Console.WriteLine(string.Join(" ", array));
-        Console.WriteLine("Test ok!");
+        Console.WriteLine("Test OK!");
         Thread.Sleep(5000);
     }
 
