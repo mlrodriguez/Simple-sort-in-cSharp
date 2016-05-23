c20286a6805b6869d3278c2c1df1ea58c4a7d5af
Marcio <marciorodriguez91@gmail.com>
2016-05-23 14:05:49 -0300

    add value to test

--- SimpleSort/Program.cs
+++ SimpleSort/Program.cs
@@ -7,7 +7,7 @@ using System.Threading;
 
 class Program {
     static void Main(string[] args) {
-        int[] array = {2,4,8,5,465,678,9,345,234,234,467,78,3,2,4,6,7,89,-10};
+        int[] array = {2,4,8,5,465,678,9,345,234,234,467,78,3,2,4,6,7,89,-10,0};
         Sort.simpleSort(array);
         Console.WriteLine(string.Join(" ", array));
         Console.WriteLine("Test OK!");
