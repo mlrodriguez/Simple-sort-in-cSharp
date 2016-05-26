af3661295ccb80e28b19d79431c65dc09c37ee0d
Marcio <marciorodriguez91@gmail.com>
2016-05-26 14:13:27 -0300

    unit tests proyect

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] array = { 2, 4, 8, 5, 465, 678, 9, 345, 234, 234, 467, 78, 3, 2, 4, 6, 7, 89, -10, 0, 100, 101 };
+            Sort.simpleSort(array);
+            for (int i = 1; i < array.Length; i++ )
+            {
+                if(array[i]<array[i-1])
+                    Assert.IsTrue(false);
+            }
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] array = { 2, 4, 8, 5, 465, 678, 9, 345, 234, 234, 467, 78, 3, 2, 4, 6, 7, 89, -10, 0, 100, 101 };
+            Sort.simpleSort(array);
+            Assert.IsTrue(array.Length==22);
+        }
+    }
+}
